1f1f809c7f168bc00b09bc5c599456ebcef1c4bf
kelvin <kelvin.xu@ubc.ca>
2024-04-25 13:53:16 -0700

    Enable interactivity API

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2ed8828d98cfbc56d0ad');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '8a410e3a1c0df754b75a');
